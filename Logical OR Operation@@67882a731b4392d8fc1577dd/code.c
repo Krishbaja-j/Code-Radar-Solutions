aa5ae16d18c960e82abb09dd499972e74297590f
Krishbaja-j <krishbajaj281@gmail.com>
2025-01-23 11:40:54 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
-int main() {
+int main(){
     int x,y;
-    scanf("%d %d",&x,&y);
-    if(x||y){
+    scanf("%d%d",&x,&y);
+    if(x>0||y>0){
         printf("True");
     }
     else{
         printf("False");
     }
-    return 0;
+
 }
