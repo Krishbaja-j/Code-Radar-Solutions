d30314510a4586b5e13f2c9f1e1a557dd04a3a07
Krish Bajaj <krishbajaj281@gmail.com>
2025-02-06 18:39:37 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char x;
+    scanf("%c",x);
+    if(x== A){
+        printf("Excellent");
+    }
+    else if(x==B){
+        printf("Good");
+    }
+    else if(x==C){
+        printf("Average");
+    }
+}
