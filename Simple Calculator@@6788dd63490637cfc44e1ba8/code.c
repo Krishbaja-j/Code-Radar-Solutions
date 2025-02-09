e5aba788b265e20f340b0450408cab69142e290d
Krish Bajaj <krishbajaj281@gmail.com>
2025-02-09 17:06:42 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-int main(){
-    int x,y;
+int main() {
+    int x, y;
     char z;
-    scanf("%d%d",&x,&y);
-    scanf("%c",&z);
-    if(z=='+'){
-        printf("%d",x+y);
+    scanf("%d %d", &x, &y);
+    getchar(); // to consume the newline character after the integers
+    scanf("%c", &z);
+    if (z == '+') {
+        printf("%d\n", x + y);
     }
+    return 0;
 }
